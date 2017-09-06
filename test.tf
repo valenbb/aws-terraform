b8d2e89e23bfb31a7bd3ed8d75046035a720b943
Andy Lalla <valenbb@gmail.com>
2017-09-05 20:43:29 -0400

    Added test terraform script

--- test.tf
+++ test.tf
@@ -3,12 +3,13 @@ provider "aws" {}
 resource "aws_instance" "test" {
   ami           = "ami-4fffc834"
   instance_type = "t2.micro"
-}
 
-tags {
+  tags {
     Name = "test"
-}
+  }
 
-security_groups = ["sg-ccc943b0"]
-availability_zone = "us-east-1b"
-subnet_id = "subnet-adbc6cf4"
+  security_groups = ["sg-ccc943b0"]
+  availability_zone = "us-east-1b"
+  subnet_id = "subnet-adbc6cf4"
+
+}
