52bcac7892c06e7c1ab01129355cd99609c1e105
valenbb <valenbb@gmail.com>
2018-09-09 15:21:38 -0400

    Updated for test server

--- test.tf
+++ test.tf
@@ -1,15 +1,79 @@
-provider "aws" {}
+provider "aws" {
+  version = "~> 1.35"
+  region = "${var.aws_region}"
+}
 
-resource "aws_instance" "test" {
-  ami           = "ami-4fffc834"
-  instance_type = "t2.micro"
+resource "aws_security_group" "test_sg" {
+  name 		= "{var.tag_name}-SG"
+  description 	= "Allow inbound traffic from home only"
+  vpc_id      	= "${var.vpc_prod_id}"
 
-  tags {
-    Name = "test"
+  ingress {
+    from_port	= 22
+    to_port	= 22
+    protocol	= "tcp"
+    cidr_blocks	= "${var.cidr_access}"
+  }
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = "${var.cidr_access}"
   }
 
-  security_groups = ["sg-d53b20b0"]
-  availability_zone = "us-east-1b"
-  subnet_id = "subnet-adbc6cf4"
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = "${var.cidr_access}"
+  }
+  
+  egress {
+    from_port 	= 0
+    to_port 	= 0
+    protocol 	= "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
+  tags {
+    Name 	= "${var.tag_name}-SG"
+    owner	= "${var.tag_owner}"
+    env		= "${var.tag_env}"
+    builder	= "${var.tag_builder}"
+  }
 }
+
+resource "aws_instance" "chef_server" {
+  ami           		= "${var.aws_ami}"
+  instance_type 		= "${var.instance_type}"
+  vpc_security_group_ids 	= ["${aws_security_group.test_sg.id}"]
+  key_name			= "${var.key_name}"  
+  availability_zone 		= "${var.az_id}"
+  subnet_id 			= "${var.subnet_id}" 
+  associate_public_ip_address 	= "${var.public_ip}"
+ 
+  tags {
+    Name 	= "${var.tag_name}-${var.tag_env}"
+    owner	= "${var.tag_owner}"
+    env		= "${var.tag_env}"
+    builder	= "${var.tag_builder}"
+  }
+
+  provisioner "chef"  {
+    connection {
+      type = "${var.connection_type}"
+      user = "${var.server_user}"
+      private_key = "${file(var.ssh_key_file)}"
+    }
+    environment = "${var.chef_environment}"
+    run_list = "${var.run_list}"
+    node_name = "${var.tag_name}-${var.tag_env}"
+    server_url = "${var.chef_server_url}"
+    recreate_client = true
+    user_name = "${var.chef_server_user_name}"
+    user_key = "${file(var.chef_server_user_key)}"
+    ssl_verify_mode = "${var.ssl_verify_mode}"
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,113 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "Desired instance type for EC2"
+  default = "t2.medium"
+}
+
+# CentOS 7 AMI
+variable "aws_ami" {
+  description = "Image Id"
+  default = "ami-0eaf7f3ca4d177abb"
+}
+
+variable "key_name" {
+  description = "Desired name of AWS key pair"
+  default = "terraform"
+}
+
+variable "vpc_prod_id" {
+  description = "Production VPC ID"
+  default = "vpc-ce4b4eb5"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID to use"
+  default = "subnet-2a26b260"
+}
+
+variable "cidr_access" {
+  type = "list"
+  description = "CIDR of originating traffic"
+  default = ["100.4.210.205/32", "165.225.38.216/32"]
+}
+
+variable "az_id" {
+  description = "Availability Zone"
+  default = "us-east-1a"
+}
+
+variable "public_ip" {
+  description = "Associate ec2 instance wth Public IP"
+  default = false
+}
+
+variable "tag_name" {
+  description = "Name tag value"
+  default = "TEST-SERVER"
+}
+
+
+variable "tag_owner" {
+  description = "Owner tag value"
+  default = "Andy Lalla"
+}
+
+variable "tag_env" {
+  description = "Environment tag value"
+  default = "DEV"
+}
+
+variable "tag_builder" {
+  description = "Provisioning tool tag value"
+  default = "Terraform"
+}
+
+variable "chef_server_url" {
+  description = "Chef Server URL"
+  default = "https://10.0.1.244/organizations/esn2"
+}
+
+variable "chef_server_user_name" {
+  description = "Account to login to Chef Server"
+  default = "terraform"
+}
+
+variable "chef_server_user_key" {
+  description = "Chef user account private key"
+  default = "/var/lib/jenkins/.chef/terraform.pem"
+}
+
+variable "server_user" {
+  description = "User account on server"
+  default = "centos"
+}
+
+variable "ssh_key_filename" {
+  description = "Server user account private key"
+  default = "/var/lib/jenkins/.ssh/terraform.pem"
+}
+
+variable "ssl_verify_mode" {
+  description = "Verify SSL on connection to chef server"
+  default = ":verify_none"
+}
+
+variable "run_list" {
+  type = "list"
+  description = "List of chef recipet to lay down on server"
+  default = ["apache::default"]
+}
+
+variable "chef_environment" {
+  description = "Chef environment"
+  default = "_default"
+}
+
+variable "connection_type" {
+  description "Connection to server"
+  default = "ssh"
+}
